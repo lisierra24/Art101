9bb4ec01cb226186f01f78d518893caf419c7ccd
lisierra24 <lisierra@ucsc.edu>
2021-01-25 20:53:48 -0800

    Update lab.js
    yeah

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+
+
+
+
+// How to create a new File in atom
+  // Open atom
+  // Make a new folder by right clicking and selecting “New Folder”
+  // In that folder, right click and select “New File”
+  // Name the file
+  // Before clicking enter, ass the file extension (.js, .html, .css)
+  // You have a new file in atom!

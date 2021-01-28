9e384da7db78696126fbbb7dbe37f9e595896de0
lisierra24 <lisierra@ucsc.edu>
2021-01-27 19:13:56 -0800

    Update lab.js
    check

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+// Author: Lizeth Sierra
+// Created: 27 January
+// License: Public Domain
+
+//Define Variable
+var make = "Jeep";
+var model = "Wrangler";
+var color = "White";
+var year = 2015;
+
+//calculations
+var age = 2021 - year;
+
+// output
+document.writenln("Make: " + make + "<br>");
+document.writenln("Model: " + model + "<br>");
+document.writenln("Color: " + color + "<br>");
+document.writenln("Year: " + year + "<br>");
+document.writenln("Age: " + age + "<br>");

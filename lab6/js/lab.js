f6482e3c65e116e772ad5e302e53ab0fea3ed74a
lisierra24 <lisierra@ucsc.edu>
2021-02-01 22:36:29 -0800

    Update lab.js
    six

---
+++ lab6/js/lab.js
@@ -0,0 +1,20 @@
+// Author: Lizeth Sierra
+// Created: 31 January
+// License: Public Domain
+
+//Define Variable
+var myTransport = ["walking", "uber", "lyft",
+  "friends' rides"]
+var myMainRide = {
+  make: "Jeep",
+  model: "Wrangler",
+  color: "White",
+  year: 2015,
+//calculations
+  age : function() {
+    return 2021-age;
+  }
+}
+
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre");

c201cfcc067448b9eeefdb28deeafd41daf283bc
lisierra24 <lisierra@ucsc.edu>
2021-02-10 21:09:11 -0800

    Create lab9.js
    lol

---
+++ lab9/js/lab9.js
@@ -0,0 +1,14 @@
+// Author: Lizeth
+// Created: 02.10.2021
+// Copyrigth by Blub Corp.
+
+var outputEl = document.getElementById("output");
+var new1El = document.createElement("p");
+new1El.innerHTML = "JavaScript for the Web";
+var new2El = document.getElementById("h1");
+new2El.innerHTML = "Lab 9";
+document.body.appendChild(new1El);
+document.body.appendChild(new2El);
+
+
+var new3El = document.getElementById("h2");

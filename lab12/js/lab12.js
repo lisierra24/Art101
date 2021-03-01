738a15224fd44e28b8a953f1d2ed447293ee9f69
Lizeth Sierra <lizethsierra@Lizeths-MacBook-Pro.local>
2021-02-28 18:54:58 -0800

    Update lab12.js
    eheheheeh

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -22,37 +22,54 @@
 // Author: Lizeth Sierra
 // Created: 02.15.2021
 // (c) COpyright by Blu Corp.
+//
+// function getHouse(num){
+//     // get the remainder divided by 4 of the length
+//     var ramainder = num % 4;
+//     // create an if else to show House
+//     if (remainder == 0) {
+//       var str = "Gryffindor";
+//     } else if (remainder == 1){
+//       var str = "Ravenclaw";
+//     }else if (remainder == 2){
+//       var str = "Hufflepuff";
+//     }else if (remainder == 3){
+//       var str = "Slytherin";
+//     }
+//
+// };
 
-function getHouse(num){
-    // get the remainder divided by 4 of the length
-    var ramainder = num % 4;
-    // create an if else to show House
-    if (remainder == 0) {
-      var str = "Gryffindor";
-    } else if (remainder == 1){
-      var str = "Ravenclaw";
-    }else if (remainder == 2){
-      var str = "Hufflepuff";
-    }else if (remainder == 3){
-      var str = "Slytherin";
-    }
+// // attach click handler to submit buttons
+// $("#button").click(function() {
+//   // get the value in the #input text box, assign a varibale name
+//   var name = $("#input").val()
+//   console.log(name);
+//   // get the length of the name
+//   var nameLenght = name.length;
+//   console.log(nameLength);
+//   //get the houses
+//   var house = getHouse(nameLength);
+//   console.log(house);
+//   // add house to outpput div
+//   $("#output").html("<h1>" + house + "</h1>");
+//   $("#output").removeClass("hidden");
+//   $("#tail-box").removeClass("hidden");
+//
+// })
 
-};
-
-// attach click handler to submit buttons
-$("#button").click(function() {
-  // get the value in the #input text box, assign a varibale name
-  var name = $("#input").val()
-  console.log(name);
-  // get the length of the name
-  var nameLenght = name.length;
-  console.log(nameLength);
-  //get the houses
-  var house = getHouse(nameLength);
-  console.log(house);
-  // add house to outpput div
-  $("#output").html("<h1>" + house + "</h1>");
-  $("#output").removeClass("hidden");
-  $("#tail-box").removeClass("hidden");
-
-})
+$("#button").click(function() {runSort()});
+function runSort() {
+  var name = $("#input").val();
+  var yourHouse = sortingHat(name);
+  var houseName = ["Gryffindor","Ravenclaw","Slytherin","Hufflepuff"];
+  $("#output").html("The sorting hat has sorted you into " + houseName[yourHouse]);
+}
+function sortingHat(string) {
+  var length = string.length;
+  if (length == 0){
+    return 0;
+  } else {
+    var mod = ((length % 4)+1);
+    return mod;
+  }
+}
